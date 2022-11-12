a14cc43d757fe02f6242098616054839b0056b50
Takumi <takumi07@students.cs.ubc.ca>
2022-11-11 18:48:05 -0800

    Made changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Popular from "./components/Popular";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Hello!!</h1>
+      <Popular />
     </div>
   );
 }
